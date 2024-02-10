e57b6389693dfb1d656ec6f1fc1fce32d8df8d11
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-10 21:12:33 +0600

    change background with the help of event

--- js/domEvents.js
+++ js/domEvents.js
@@ -1,1 +1,10 @@
-console.log("this is a seperate js file");
+const makeBlue = document.getElementById("make-blue");
+makeBlue.addEventListener("click", function () {
+  document.body.style.backgroundColor = "blue";
+});
+
+const makePurple = document.getElementById("make-purple");
+makePurple.onclick = makePu();
+function makePu() {
+  document.body.backgroundColor = "purple";
+}
